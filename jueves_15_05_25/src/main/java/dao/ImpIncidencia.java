06592ee806612416cd3917d0d2f967a4dddebfde
alumno <alumno@dam203>
2025-05-15 11:07:27 +0200

    Base 15-05-2025

--- jueves_15_05_25/src/main/java/dao/ImpIncidencia.java
+++ jueves_15_05_25/src/main/java/dao/ImpIncidencia.java
@@ -1,8 +1,13 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import modelo.Incidencia;
 
 public class ImpIncidencia implements IncidenciaDao {
+	
+	private final EntityManagerFactory emf;
+	private final EntityManager em;
 
 	@Override
 	public boolean crear(Incidencia i) {
